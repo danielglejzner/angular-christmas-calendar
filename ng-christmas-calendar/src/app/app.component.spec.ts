1ba60f1be8f39222a7426ea44da8c6bd50efb06a
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-11-23 12:21:46 +0100

    feat: remove ng template

--- ng-christmas-calendar/src/app/app.component.spec.ts
+++ ng-christmas-calendar/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'angular-christmas-calendar' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-christmas-calendar');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-christmas-calendar');
-  });
 });
