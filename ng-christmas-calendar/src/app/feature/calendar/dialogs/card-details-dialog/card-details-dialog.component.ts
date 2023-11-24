d76a36ddfe85e19020d728e6e9651749e0c63636
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-11-24 13:56:53 +0100

    feat: style dialog

--- ng-christmas-calendar/src/app/feature/calendar/dialogs/card-details-dialog/card-details-dialog.component.ts
+++ ng-christmas-calendar/src/app/feature/calendar/dialogs/card-details-dialog/card-details-dialog.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { UiCalendarCard } from '../../interfaces/christmas-calendar-data';
 import { TwitterUrlPipe } from '../../pipes/twitter-url.pipe';
+import { CalendarCardFactoryDirective } from '../../cards/calendar-card-factory.directive';
 
 @Component({
   selector: 'xmas-card-details-dialog',
   standalone: true,
-  imports: [CommonModule, TwitterUrlPipe],
+  imports: [CommonModule, CalendarCardFactoryDirective, TwitterUrlPipe],
   templateUrl: './card-details-dialog.component.html',
   styleUrl: './card-details-dialog.component.css'
 })
