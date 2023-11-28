9979fabd3c7510133ab5318d47325339bf82a1d1
Paweł Kubiak <pawel.kubiak.dev@gmail.com>
2023-11-28 11:57:44 +0100

    feat: amend reveal logic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,12 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {
-  END_DATE,
-  OVERRIDE_CARD_INDEX_TO_REVEAL,
-  START_DATE,
-} from './feature/calendar/providers/date-range';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    {provide: START_DATE, useValue: new Date(2023, 11, 1)},
-    {provide: END_DATE, useValue: new Date(2023, 11, 24)},
-    // TODO: remove once implementation will be done
-    {provide: OVERRIDE_CARD_INDEX_TO_REVEAL, useValue: 3},
+    // Note: It could be useful to define which card should have reveal ability.
+    // {provide: OVERRIDE_CARD_INDEX_TO_REVEAL, useValue: 2},
   ],
 };
