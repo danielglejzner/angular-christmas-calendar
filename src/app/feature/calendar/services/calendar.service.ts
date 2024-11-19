25524fe71154172c7516a77b0f2b8530c728ce64
Daniel Glejzner <69144223+danielglejzner@users.noreply.github.com>
2024-11-19 20:17:10 +0100

    fix: fix calendar card doors display bugs

--- src/app/feature/calendar/services/calendar.service.ts
+++ src/app/feature/calendar/services/calendar.service.ts
@@ -1,7 +1,7 @@
-import {Injectable, inject} from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import calendarData from './../../../../assets/content/angular-calendar.json';
-import {UiCalendarCard} from '../interfaces/christmas-calendar-data';
-import {OVERRIDE_CARD_INDEX_TO_REVEAL} from '../providers/date-range';
+import { UiCalendarCard } from '../interfaces/christmas-calendar-data';
+import { OVERRIDE_CARD_INDEX_TO_REVEAL } from '../providers/date-range';
 
 @Injectable({
   providedIn: 'root',
@@ -10,12 +10,17 @@ export class CalendarService {
   private readonly overrideCardIndexToReveal = inject(OVERRIDE_CARD_INDEX_TO_REVEAL, {
     optional: true,
   });
-  private readonly cardIndexToReveal =
-    this.overrideCardIndexToReveal ?? calendarData.data.filter((day) => !!day.contents).length;
 
-  readonly cards: UiCalendarCard[] = calendarData.data.map((card) => ({
-    ...card,
-    revealed: card.day < this.cardIndexToReveal,
-    canReveal: card.day === this.cardIndexToReveal,
-  }));
+  private readonly cardIndexToReveal =
+    this.overrideCardIndexToReveal ??
+    calendarData.data.findIndex((day) => day.contents.length > 0);
+
+  readonly cards: UiCalendarCard[] = calendarData.data.map((card) => {
+    const hasContents = card.contents.length > 0;
+    return {
+      ...card,
+      revealed: hasContents && card.day < this.cardIndexToReveal,
+      canReveal: hasContents && card.day === this.cardIndexToReveal,
+    };
+  });
 }
