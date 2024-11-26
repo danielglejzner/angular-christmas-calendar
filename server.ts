46caddd9a59a443fb71e34fad48355a04cad50eb
abdelkaderbelgheddouche <abelgheddouche@gmail.com>
2024-11-26 23:36:59 +0100

    fix netlify

--- server.ts
+++ server.ts
@@ -1,9 +1,25 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr/node';
+// @ts-ignore
+import { render } from '@netlify/angular-runtime/common-engine';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+
+
+const commonEngine = new CommonEngine()
+
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine)
+}
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
