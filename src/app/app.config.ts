176e6f3c390ae04e92f6f7e915a94ec3d8a6efd7
Yberion <Brandon.Largeau@gmail.com>
2024-11-29 17:26:45 +0100

    feat: update app config to use eventCoalescing & withEventReplay

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
     // Note: It could be useful to define which card should have reveal ability.
     // {provide: OVERRIDE_CARD_INDEX_TO_REVEAL, useValue: 2},
   ],
