948af5feec9416554aa4295ce966f2ee7836e36a
Yberion <Brandon.Largeau@gmail.com>
2024-11-29 14:34:15 +0100

    refactor: remove eslint

--- src/app/feature/calendar/directives/open-details-dialog.directive.ts
+++ src/app/feature/calendar/directives/open-details-dialog.directive.ts
@@ -1,7 +1,7 @@
-import {Directive, Input, inject} from '@angular/core';
-import {UiCalendarCard} from '../interfaces/christmas-calendar-data';
-import {Dialog} from '@angular/cdk/dialog';
-import {CardDetailsDialogComponent} from '../dialogs/card-details-dialog/card-details-dialog.component';
+import { Dialog } from '@angular/cdk/dialog';
+import { Directive, Input, inject } from '@angular/core';
+import { CardDetailsDialogComponent } from '../dialogs/card-details-dialog/card-details-dialog.component';
+import { UiCalendarCard } from '../interfaces/christmas-calendar-data';
 
 @Directive({
   selector: '[openDetailsDialog]',
@@ -24,7 +24,6 @@ export class OpenDetailsDialogDirective {
       this.openDialog();
     } else if (this.card.canReveal) {
       // Note: we used set timeout because we would like to display 'open cards door' animation, otherwise it'll be overlapped by dialog.
-      // eslint-disable-next-line angular/timeout-service
       setTimeout(() => {
         if (this.dialog.openDialogs.length === 0 && this.card.revealed) {
           this.openDialog();
